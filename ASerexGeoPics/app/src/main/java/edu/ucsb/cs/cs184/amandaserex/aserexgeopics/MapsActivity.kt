419c1baca70f752d672cfab62236aed7088ea302
Amanda Serex <amandaserex@ucsb.edu>
2020-12-16 21:29:48 -0800

    Likes dont work quite right but heart fab implemented

--- ASerexGeoPics/app/src/main/java/edu/ucsb/cs/cs184/amandaserex/aserexgeopics/MapsActivity.kt
+++ ASerexGeoPics/app/src/main/java/edu/ucsb/cs/cs184/amandaserex/aserexgeopics/MapsActivity.kt
@@ -1,11 +1,19 @@
 package edu.ucsb.cs.cs184.amandaserex.aserexgeopics
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -16,6 +24,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -36,10 +45,63 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         var comment = ""
         var latitude = 0.0
         var longitude = 0.0
-        var likes = 0
+        var likes = "0.0"
         var image = ""
         var addNew = false
+        var firebaseID = ""
+        var tempLikes = 0
     }
+
+    inner class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapter {
+
+        var mContext = context
+        var mWindow = (context as Activity).layoutInflater.inflate(R.layout.infowindow, null)
+
+
+
+        private fun rendowWindowText(marker: Marker, view: View) {
+            GlobalVars.image = marker.snippet
+            val tvTitle = view.findViewById<TextView>(R.id.title)
+            tvTitle.text = marker.title
+            val picture = view.findViewById<ImageView>(R.id.picture)
+            picture.setImageBitmap(BitmapFactory.decodeFile(marker.snippet, BitmapFactory.Options()))
+            val likeButton = view.findViewById<TextView>(R.id.likeButton)
+            likeButton.text = marker.zIndex.toInt().toString()
+
+
+
+            val database = Firebase.database
+            val myRef = database.getReference("points").orderByKey()
+
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (postSnapshot in dataSnapshot.children) {
+                        val options: BitmapFactory.Options = BitmapFactory.Options()
+                        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                        if (GlobalVars.image == postSnapshot.child("image").value){
+                            likeButton.text = postSnapshot.child("likes").value.toString()
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+
+        override fun getInfoContents(marker: Marker): View {
+            rendowWindowText(marker, mWindow)
+            return mWindow
+        }
+
+        override fun getInfoWindow(marker: Marker): View? {
+            rendowWindowText(marker, mWindow)
+            return mWindow
+        }
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,7 +112,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     Log.e("DEBUG", "${it.key} = ${it.value}")
                     if (it.key == "android.permission.ACCESS_FINE_LOCATION") {
                         if (it.value) {
-                            mMap.setMyLocationEnabled(true);
+                            mMap.setMyLocationEnabled(true)
                         }
                     } else {
                         if (it.value) {
@@ -60,20 +122,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
 
-
         if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
         ) {
             requestMultiplePermissions.launch(
-                arrayOf(
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
+                    arrayOf(
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
             )
         }
 
@@ -90,20 +155,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (postSnapshot in dataSnapshot.children) {
                     val name = postSnapshot.child("name").getValue(String::class.java)
-                    if (name == Login.GlobalVars.userName) {
-                        mMap.addMarker(
-                            MarkerOptions()
-                                .position(
-                                    LatLng(
-                                        postSnapshot.child("latitude")
-                                            .getValue() as Double,
-                                        postSnapshot.child(
-                                            "longitude"
-                                        ).getValue() as Double
-                                    )
-                                )
-                                .title(postSnapshot.child("comment").getValue() as String?)
-                        )
+                    val options: BitmapFactory.Options = BitmapFactory.Options()
+                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    if (name == Login.GlobalVars.userName){
+                        if(postSnapshot.child("likes").value != null) {
+                            mMap.addMarker(
+                                    MarkerOptions()
+                                            .position(
+                                                    LatLng(
+                                                            postSnapshot.child("latitude")
+                                                                    .getValue() as Double,
+                                                            postSnapshot.child(
+                                                                    "longitude"
+                                                            ).getValue() as Double
+                                                    )
+                                            )
+                                            .snippet(postSnapshot.child("image").value.toString())
+                                            .title(postSnapshot.child("comment").value as String?)
+                                            .zIndex((postSnapshot.child("likes").value as String).toFloat())
+                            )
+                        }
                     }
                 }
             }
@@ -115,7 +186,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 // ...
             }
         })
-
     }
 
     override fun onResume() {
@@ -126,6 +196,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val myRef = database.getReference("points")
 
             val newPoint = myRef.push()
+            val fbId = newPoint.child("firebaseID")
+            fbId.setValue(newPoint.key)
+            GlobalVars.firebaseID = newPoint.key.toString()
             val lat = newPoint.child("latitude")
             lat.setValue(GlobalVars.latitude.toDouble())
             val long = newPoint.child("longitude")
@@ -135,9 +208,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val comment = newPoint.child("comment")
             comment.setValue(GlobalVars.comment)
             val likes = newPoint.child("likes")
-            likes.setValue(0)
+            likes.setValue("0.0")
+            GlobalVars.tempLikes = 0
+            val image = newPoint.child("image")
+            image.setValue(GlobalVars.image)
         }
     }
+
 
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -145,45 +222,73 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         if(GlobalVars.addNew){
             mMap.addMarker(
-                MarkerOptions()
-                    .position(
-                        LatLng(
-                            GlobalVars.latitude,
-                            GlobalVars.longitude
-                        )
-                    )
-                    .title(GlobalVars.comment)
+                    MarkerOptions()
+                            .position(
+                                    LatLng(
+                                            GlobalVars.latitude,
+                                            GlobalVars.longitude
+                                    )
+                            )
+                            .snippet(GlobalVars.image)
+                            .title(GlobalVars.comment)
+                            .zIndex(0.0F)
             )
             GlobalVars.addNew = false
         }
 
+        val heart: FloatingActionButton = findViewById<FloatingActionButton>(R.id.fabHeart)
+
         val fab: FloatingActionButton = findViewById<FloatingActionButton>(R.id.fab)
 
+        heart.setOnClickListener { view ->
+            if(GlobalVars.image == ""){
 
-        val database = Firebase.database
-        val myRef = database.getReference("points")
+            }
+            else{
+
+                val database = Firebase.database
+                val myRef = database.getReference("points").orderByKey()
+                myRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        for (postSnapshot in dataSnapshot.children) {
+                            val options: BitmapFactory.Options = BitmapFactory.Options()
+                            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                            if (GlobalVars.image == postSnapshot.child("image").value){
+                                GlobalVars.firebaseID = postSnapshot.key.toString()
+                                GlobalVars.likes = postSnapshot.child("likes").value.toString()
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+                GlobalVars.tempLikes = (GlobalVars.likes.toFloat()+1).toInt()
+                database.getReference("points").child(GlobalVars.firebaseID).child("likes").setValue((GlobalVars.likes.toFloat()+1).toString())
+            }
+        }
 
 
-        val fremont = LatLng(37.5485, -121.9886)
-        mMap.addMarker(MarkerOptions().position(fremont).title("Marker in Fremont"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fremont, 14.0f))
 
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.5485, -121.9886), 14.0f))
 
         fab.setOnClickListener { view ->
 
 
             if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
             ) {
                 Snackbar.make(
-                    view,
-                    "You are unable to use this feature. Please allow access to your location and camera in settings",
-                    Snackbar.LENGTH_LONG
+                        view,
+                        "You are unable to use this feature. Please allow access to your location and camera in settings",
+                        Snackbar.LENGTH_LONG
                 )
                     .setAction("Action", null).show()
             }
@@ -197,5 +302,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val i = Intent(this, PostActivity::class.java)
             startActivity(i)
         }
+        mMap.setInfoWindowAdapter(CustomInfoWindowForGoogleMap(this))
     }
+
 }
+
--- ASerexGeoPics/app/src/main/java/edu/ucsb/cs/cs184/amandaserex/aserexgeopics/PostActivity.kt
+++ ASerexGeoPics/app/src/main/java/edu/ucsb/cs/cs184/amandaserex/aserexgeopics/PostActivity.kt
@@ -1,12 +1,23 @@
 package edu.ucsb.cs.cs184.amandaserex.aserexgeopics
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 class PostActivity : AppCompatActivity() {
 
@@ -16,16 +27,81 @@ class PostActivity : AppCompatActivity() {
 
         val edit: EditText = findViewById<EditText>(R.id.editText)
         val addButton: Button = findViewById<Button>(R.id.add)
+        val cancelButton: Button = findViewById<Button>(R.id.cancelButton)
+        val takePhoto: ImageView = findViewById<ImageView>(R.id.imageView2)
+
+        val REQUEST_IMAGE_CAPTURE = 1
+
+        fun dispatchTakePictureIntent(): Boolean {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+                return false
+            }
+            return true
+        }
+
+
+        takePhoto.setOnClickListener(View.OnClickListener() {
+            dispatchTakePictureIntent()
+        })
+
+
+        cancelButton.setOnClickListener(View.OnClickListener {
+            val i = Intent(this, MapsActivity::class.java)
+            startActivity(i)
+        })
 
 
         addButton.setOnClickListener(View.OnClickListener {
-            //need to add photo
             MapsActivity.GlobalVars.comment = edit.text.toString()
             MapsActivity.GlobalVars.addNew = true
             val i = Intent(this, MapsActivity::class.java)
             startActivity(i)
         })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val takePhoto: ImageView = findViewById<ImageView>(R.id.imageView2)
+
+        fun createImageFile(){
+            val drawable : BitmapDrawable = takePhoto.drawable as BitmapDrawable
+            val bitmap : Bitmap = drawable.bitmap
+            val filepath : File = Environment.getExternalStorageDirectory()
+            val dir : File = File(filepath.toString() + "/ASerexGeoPics/")
+            if(dir.mkdir()){
+                Log.e("TAG", "Made directory")
+            }
+            else{
+                Log.e("TAG", dir.exists().toString())
+            }
+            val imageName = System.currentTimeMillis().toString() + ".jpeg"
+            val file : File = File(dir, imageName)
+            MapsActivity.GlobalVars.image = file.toString()
+            Log.e("DIRECTORY", dir.toString())
+            Log.e("FILE", file.toString())
+            try {
+                val outputStream : OutputStream = FileOutputStream(file)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
+            } catch (e: FileNotFoundException){
+                Log.e("TAG", "File Not Found")
+                e.printStackTrace()
+            }
+        }
+
+
+        if (resultCode == RESULT_OK) {
+                val photo: Bitmap = data?.extras?.get("data") as Bitmap
+                takePhoto.setImageBitmap(photo)
+                createImageFile()
+            }
+
+        }
 
 
 }
